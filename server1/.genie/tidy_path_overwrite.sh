6e14850197c37c56a66bda999009507520566b50
Tatsuya Karino <karino@kazaoki.jp>
2016-02-07 22:27:56 +0900

    alpine で php コンパイルが通った！

--- server1/.genie/tidy_path_overwrite.sh
+++ server1/.genie/tidy_path_overwrite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's/buffio.h/tidybuffio.h/' /tmp/php-build/source/*/ext/tidy/*.c
+sed -i 's/"buffio.h"/"tidybuffio.h"/' /tmp/php-build/source/*/ext/tidy/*.c
 sed -i '/\-\-with\-tidy/d' /root/.anyenv/envs/phpenv/plugins/php-build/share/php-build/default_configure_options
 sed -i '/\-\-with\-apxs2/d' /root/.anyenv/envs/phpenv/plugins/php-build/share/php-build/default_configure_options
 echo '--with-tidy=/usr/local' >> /root/.anyenv/envs/phpenv/plugins/php-build/share/php-build/default_configure_options
