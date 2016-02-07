f2b41cca427faf332a5f95750d01406ec0da37e4
Tatsuya Karino <karino@kazaoki.jp>
2016-02-07 11:03:38 +0900

    anyenvが安定してきた。

--- server1/.genie/config.pl
+++ server1/.genie/config.pl
@@ -21,6 +21,35 @@ return {
 		],
 	},
 
+	# ポートバインド設定
+	# ------------------
+	# 未指定の場合は開いてるポートが適用されます。
+	PORTS => {
+		# 80   => 80,
+		# 8080 => 8080,
+		# 443  => 443,
+		# 3306 => 3306,
+		# 5432 => 5432,
+	},
+
+	# Perl設定
+	# --------
+	PERL => {
+		VERSION => '', # `plenv install -l` でリストアップされるバージョン文字列を指定
+	},
+
+	# Ruby設定
+	# --------
+	RUBY => {
+		VERSION => '', # `rbenv install -l` でリストアップされるバージョン文字列を指定
+	},
+
+	# PHP設定
+	# --------
+	PHP => {
+		VERSION => '', # `phpenv install -l` でリストアップされるバージョン文字列を指定
+	},
+
 	# Apache設定
 	# ----------
 	APACHE => {
@@ -33,5 +62,20 @@ return {
 	POSTFIX => {
 		FORCE_ENVELOPE => 'xxx@xxx.xxx',
 	},
+
+	# MySQL設定
+	# ---------
+	MYSQL => [
+		# 1つめ
+		{
+			type => 'inner', # kazaoki/genieコンテナ内蔵のMySQLを使用
+			name => 'main', # 
+		},
+		# 2つめ
+		{
+			type => 'outer', # 外部コンテナのMySQLを使用
+			name=>
+		},
+	],
 
 };
