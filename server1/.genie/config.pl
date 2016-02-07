181476d4fdaa25415c6cb8dc1e8e4ae2018e1e30
Tatsuya Karino <karino@kazaoki.jp>
2016-02-07 11:23:39 +0900

    Dockerfileの修正と、configの作りこみ進め

--- server1/.genie/config.pl
+++ server1/.genie/config.pl
@@ -21,16 +21,16 @@ return {
 		],
 	},
 
-	# ポートバインド設定
-	# ------------------
-	# 未指定の場合は開いてるポートが適用されます。
-	PORTS => {
-		# 80   => 80,
-		# 8080 => 8080,
-		# 443  => 443,
-		# 3306 => 3306,
-		# 5432 => 5432,
-	},
+	# ポート開放設定
+	# --------------
+	# '80:80'と指定することで強制バインド可能
+	PORTS => [
+		80,
+		8080,
+		443,
+		3306,
+		5432,
+	],
 
 	# Perl設定
 	# --------
@@ -53,6 +53,15 @@ return {
 	# Apache設定
 	# ----------
 	APACHE => {
+		ENABLED => 1,
+		PUBLIC_PATH => '../www',
+		BANDWIDTH => '',
+	},
+
+	# Nginx設定
+	# ----------
+	NGINX => {
+		ENABLED => 1,
 		PUBLIC_PATH => '../www',
 		BANDWIDTH => '',
 	},
@@ -60,22 +69,67 @@ return {
 	# Postfix設定
 	# -----------
 	POSTFIX => {
+		ENABLED => 1,
 		FORCE_ENVELOPE => 'xxx@xxx.xxx',
 	},
 
 	# MySQL設定
 	# ---------
-	MYSQL => [
-		# 1つめ
-		{
-			type => 'inner', # kazaoki/genieコンテナ内蔵のMySQLを使用
-			name => 'main', # 
-		},
-		# 2つめ
-		{
-			type => 'outer', # 外部コンテナのMySQLを使用
-			name=>
-		},
-	],
+	MYSQL => {
+		ENABLED => 1,
+		DATABASES => [
+			# 内部コンテナのMySQLを利用
+			{
+				NAME => genie_db1,
+				USER => genie_user1,
+				PASS => 123456789,
+				# HOSTNAME_TO_LOCAL => mysql999.db.com,
+				CHARSET => UTF8,
+			},
+			# 外部コンテナのMySQLを利用（officialのみ対応：https://hub.docker.com/_/mysql/）
+			{
+				IMAGE => 'mysql:5.5',
+				NAME => genie_db2,
+				USER => genie_user2,
+				PASS => 123456789,
+				# HOSTNAME_TO_LOCAL => mysql999.db.com,
+				CHARSET => UTF8,
+			},
+		],
+		# -- 外部MySQLコンテナ起動（officialのみ対応：https://hub.docker.com/_/mysql/）
+		OUTER_DBS => [
+			# 1つめ
+			{
+				IMAGE => 'mysql:5.5',
+				NAME => genie_db1,
+				USER => genie_user1,
+				PASS => 123456789,
+				# HOSTNAME_TO_LOCAL => mysql999.db.com,
+				CHARSET => UTF8,
+			},
+			# 2つめ
+			{
+				IMAGE => 'mysql:5.5',
+				NAME => genie_db2,
+				USER => genie_user2,
+				PASS => 123456789,
+				# HOSTNAME_TO_LOCAL => mysql999.db.com,
+				CHARSET => UTF8,
+			},
+		],
+	},
+
+
+# # [PostgreSQL]
+# LAMP_PGSQL=0
+# LAMP_PGSQL_DB=lamp
+# LAMP_PGSQL_USER=lamp
+# LAMP_PGSQL_PASS=123456
+# LAMP_PGSQL_PORT=5432:5432
+# # LAMP_PGSQL_HOSTNAME_TO_LOCAL=pgsql3.db
+# LAMP_PGSQL_ENCODING=utf8
+# LAMP_PGSQL_LC_COLLATE=ja_JP.utf-8
+# LAMP_PGSQL_LC_CTYPE=ja_JP.utf-8
+
 
 };
