33ca68b7fd3bd0886f5da3df4d54e668166f22d8
Tatsuya Karino <karino@kazaoki.jp>
2016-02-13 21:55:58 +0900

    PHPバージョン指定の途中

--- server1/.genie/entry.sh
+++ server1/.genie/entry.sh
@@ -19,15 +19,15 @@ echo ". /etc/bashrc" >> /root/.bashrc
 # if [[ $GENIE_PHP_VERSION != '' ]]; then
 #   php_install_path="/genie/service/php/$GENIE_PHP_VERSION/"
 #   if [[ ! -e $php_install_path ]]; then
-#     sed -i -e '1i configure_option "--with-apxs2" "/usr/sbin/apxs"' /usr/local/share/php-build/definitions/$LAMP_PHP_VERSION
-#     php-build $LAMP_PHP_VERSION /opt/php-$LAMP_PHP_VERSION
-#     ln -s /opt/php-$LAMP_PHP_VERSION /root/.phpenv/versions/$LAMP_PHP_VERSION
-#     phpenv global $LAMP_PHP_VERSION
+#     sed -i -e '1i configure_option "--with-apxs2" "/usr/bin/apxs"' /root/.anyenv/envs/phpenv/plugins/php-build/share/php-build/definitions/$GENIE_PHP_VERSION
+#     ~/.anyenv/envs/phpenv/plugins/php-build/bin/php-build $GENIE_PHP_VERSION /genie/service/php/$GENIE_PHP_VERSION
+#     ln -s /genie/service/php/$GENIE_PHP_VERSION /root/.phpenv/versions/$GENIE_PHP_VERSION
+#     phpenv global $GENIE_PHP_VERSION
 #     phpenv rehash
-#     cp /etc/httpd/modules/libphp5.so /root/.phpenv/versions/$LAMP_PHP_VERSION/
+#     cp /etc/httpd/modules/libphp5.so /root/.phpenv/versions/$GENIE_PHP_VERSION/
 #   else
-#     ln -s /opt/php-$LAMP_PHP_VERSION /root/.phpenv/versions/$LAMP_PHP_VERSION
-#     cp /root/.phpenv/versions/$LAMP_PHP_VERSION/libphp5.so /etc/httpd/modules/
+#     ln -s /genie/service/php/$GENIE_PHP_VERSION /root/.phpenv/versions/$GENIE_PHP_VERSION
+#     cp /root/.phpenv/versions/$GENIE_PHP_VERSION/libphp5.so /etc/httpd/modules/
 #   fi
 #   service httpd start
 # fi
