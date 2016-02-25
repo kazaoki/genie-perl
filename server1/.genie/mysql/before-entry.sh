3498f9bafc75968c4a0da33cd359f4c92e46b386
Tatsuya Karino <karino@kazaoki.jp>
2016-02-25 21:34:25 +0900

    MySQLのバックアップをダンプ方式に変更。

--- server1/.genie/mysql/before-entry.sh
+++ server1/.genie/mysql/before-entry.sh
@@ -12,6 +12,14 @@ sed -i "s/<__MYSQL_CHARSET__>/$MYSQL_CHARSET/" /etc/mysql/conf.d/*
 sed -i "s/<__MYSQL_PORT__>/$MYSQL_PORT/" /etc/mysql/conf.d/*
 chmod -R 0660 /etc/mysql/conf.d
 
+# Copy shell file
+# ---------------
+cp /genie/mysql/docker-entrypoint-initdb.d/* /docker-entrypoint-initdb.d
+
+# Copy dump file
+# --------------
+cp /genie/mysql/dumps/$MYSQL_LABEL.sql /docker-entrypoint-initdb.d
+
 # Pass to true shell
 # ------------------
 exec /entrypoint.sh $@
--- server1/.genie/mysql/docker-entrypoint-initdb.d/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Extact exist db data
-# --------------------
-if [ -f /genie/mysql/dbdata_$MYSQL_LABEL.tar.gz ]; then
-  mkdir -p /var/lib/mysql/$MYSQL_DATABASE
-  tar xfz /genie/mysql/dbdata_$MYSQL_LABEL.tar.gz -C /var/lib/mysql/$MYSQL_DATABASE
-fi
